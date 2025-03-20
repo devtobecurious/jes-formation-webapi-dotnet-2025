6ae6a25a17b62ce2600123c18c0258c681a117c3
evan boissonnot <evan@boissonnot.fr>
2025-03-20 11:03:43 +0100

    feat: properties to videogames

---
+++ DTBC.Ludotek.Core.VideoGames.Models/VideoGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTBC.Ludotek.Core.VideoGames.Models
+{
+	public class VideoGame
+	{
+		#region Properties
+		public int Id { get; set; }
+
+		public required string Title { get; set; }
+
+		private string description = string.Empty;
+		public string Description 
+		{ 
+			get => description;
+			set 
+			{
+				if(string.IsNullOrEmpty(value))
+				{
+					throw new ArgumentException("Description cannot be null or empty");
+				}
+				this.description = value;
+			} 
+				
+		#endregion
+	}
+}
