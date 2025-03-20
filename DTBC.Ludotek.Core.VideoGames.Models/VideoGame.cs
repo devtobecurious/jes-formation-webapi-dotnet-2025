e5fb0d9d022f28f5bfe6628859ed6fb7d24af798
evan boissonnot <evan@boissonnot.fr>
2025-03-20 11:11:16 +0100

    feat: use field / init and required

--- DTBC.Ludotek.Core.VideoGames.Models/VideoGame.cs
+++ DTBC.Ludotek.Core.VideoGames.Models/VideoGame.cs
@@ -13,17 +13,19 @@ namespace DTBC.Ludotek.Core.VideoGames.Models
 
 		public required string Title { get; set; }
 
-		private string description = string.Empty;
+		public int Year { get; init; }
+
+		//private string description = string.Empty;
 		public string Description 
-		{ 
-			get => description;
+		{
+			get; // => description;
 			set 
 			{
 				if(string.IsNullOrEmpty(value))
 				{
 					throw new ArgumentException("Description cannot be null or empty");
 				}
-				this.description = value;
+				field = value;
 			} 
 				
 		#endregion
